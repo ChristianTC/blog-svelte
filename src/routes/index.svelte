4fe7f2de3d451c10c7942a7b7fd5011b0a729daa
ChristianTC <christiantambo15@gmail.com>
2020-11-26 23:54:33 -0400

    component Main created and modified, it has been incluided in _layout.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -40,11 +40,7 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="Home">
+	Blogposts
+</div>
