1c5e0abd9fe8d70d40bcef539d99fdbcab983a0f
ChristianTC <christiantambo15@gmail.com>
2020-11-27 16:03:35 -0400

    Posts incluided at index.svelte and component Post has been created

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,34 @@
+<script context="module">
+	export function preload() {
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
 <script>
-	import successkid from 'images/successkid.jpg';
+	import Post from '../components/Post.svelte';
+	export let posts;
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
+	.Posts {
+		display: grid;
+		justify-content: space-between;
+		grid-gap: 15px;
+		grid-template-columns: 1fr;
 	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Christian Tambo</title>
 </svelte:head>
 
 
 <div class="Home">
-	Blogposts
+	<div class="Posts">
+		{#each posts as post}
+			<Post {post} />
+		{/each}
+	</div>
 </div>
