8884916cc12af8d986ee4735b9b41984d5d4e70e
ChristianTC <christiantambo15@gmail.com>
2020-11-27 13:49:51 -0400

    Component Header created and Header design and layout completed

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Nav from '../components/Nav.svelte';
 	import Main from '../components/Main.svelte';
-	
+	import Header from '../components/Header.svelte';
+
 	export let segment;
 </script>
+<Header />
 
 <Nav {segment}/>
 
