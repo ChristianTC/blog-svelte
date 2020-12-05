7af408b2b6ac8a00673be4cf69dde781a93ebf10
ChristianTC <christiantambo15@gmail.com>
2020-12-04 20:00:59 -0400

    Implementing Ghost in the Blog

---
+++ src/bin/index.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+let postsObj = require('../routes/blog/_posts.json');
+
+require('dotenv').config();
+
+const API = process.env.GHOST_API;
+
+const writeFile = (obj) => {
+    const parseData = JSON.stringify(obj);
+    fs.writeFileSync('./src/routes/blog/_posts.json', parseData);
+    console.log('Datos guardados');
+}
+
+const fetchData = async () => {
+    const response = await fetch(API);
+    const data = await response.json();
+    const posts = await data.posts.map(post => {
+        return {
+            "title": post.title,
+            "html": post.html,
+            "slug": post.slug,
+            "createdAt": post.created_at,
+            "id": post.id,
+            "desc": post.custom_excerpt,
+            "tag": post.meta_title,
+            image: post.feature_image
+        }
+    });
+
+    if (postsObj.length >= 15){
+        if (posts[0].title===postsObj[0].title) {
+            postsObj.shift();
+            postsObj.unshift(posts[0]);
+            writeFile(postsObj);
+        } else {
+            postsObj.unshift(posts[0]);
+            writeFile(postsObj);
+        }
+    } else {
+        writeFile(posts);
+    }
+}
+
+fetchData();
--- src/routes/general.svelte
+++ src/routes/general.svelte
@@ -1,3 +1,26 @@
+<script context="module">
+	export function preload() {
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
+<style>
+    .Posts {
+        display: grid;
+        justify-content: space-between;
+        grid-gap: 30px;
+        grid-template-columns: 1fr;
+    }
+</style>
+
+<script>
+    import Post from '../components/Post.svelte';
+    export let posts;
+    const filterPost = posts.filter(post => post.tag === 'general');
+</script>
+
 <svelte:head>
     <title>
         General
@@ -5,5 +28,14 @@
 </svelte:head>
 
 <div class="General">
-    <h2>General</h2>
+    <h1>General</h1>
+    <div class="Posts">
+        {#if filterPost.length >= 1}
+            {#each filterPost as post}
+                <Post {post} />
+            {/each}
+        {:else}
+            <p>Sin resultados ...</p>
+        {/if}
+    </div>
 </div>
